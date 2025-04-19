e56f404591cc9c5faa66c65a4232cec776c9f250
VArch <farrelnikoson@gmail.com>
2025-04-19 18:32:00 +0700

    a

---
+++ backend-communication-design-pattern/multiplexing-demultiplexing/server.js
@@ -0,0 +1,38 @@
+const app = require("express")()
+
+const jobs = {}
+
+app.post("/submit", (_, res) => {
+    const jobId = `job:${Date.now()}`
+    jobs[jobId] = 0
+    updateJob(jobId, 0)
+    res.setHeader(`Content-Security-Policy`, `default-src 'self'`)
+    res.setHeader(`Content-Type`, `application/json`)
+    res.write()
+    res.end("\n\n" + jobId + "\n\n")
+})
+
+app.get("/checkstatus", async (req, res) => {
+    console.log(jobs[req.query.jobId])
+    while(await checkJobCompleted(req.query.jobId) == false); // must use ; or not working
+    res.end("\n\nJobStatus: Complete " + jobs[req.query.jobId] + "%\n\n")
+})
+
+app.listen(8080, () => console.log("listening on 8080"))
+
+async function checkJobCompleted(jobId) {
+    return new Promise((resolve, _) => {
+        if (jobs[jobId] < 100) {
+            this.setTimeout(() => resolve(false), 1000)
+        } else {
+            resolve(true)
+        }
+    })
+}
+
+function updateJob(jobId, prg) {
+    jobs[jobId] = prg
+    console.log(`updated ${jobId} to ${prg}`)
+    if (prg === 100) return
+    this.setTimeout(() => updateJob(jobId, prg + 10), 3000)
+}
